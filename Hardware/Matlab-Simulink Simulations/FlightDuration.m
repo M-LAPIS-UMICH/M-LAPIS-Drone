b984ec0004656d3d04744df7020cb60bcf6fbcad
Gabriel Stavrakas <gabstav@umich.edu>
2024-11-12 18:47:31 -0500

    Final update and flight duration function creation

--- Hardware/Matlab-Simulink Simulations/FlightDuration.m
+++ Hardware/Matlab-Simulink Simulations/FlightDuration.m
@@ -16,23 +16,41 @@ if ~isempty(nanIndex)
     pwr = pwr(1:nanIndex-1);
 end
 
-threshold = 100;
-indeces = throttle <= threshold;
+upper_threshold = 100;
+upper_indeces = throttle <= upper_threshold;
 
-throttle = throttle(indeces);
-pwr = pwr(indeces);
-thrust = thrust(indeces);
+throttle = throttle(upper_indeces);
+pwr = pwr(upper_indeces);
+thrust = thrust(upper_indeces);
+
+lower_threshold = 20;
+lower_indeces = throttle > lower_threshold;
+
+throttle = throttle(lower_indeces);
+pwr = pwr(lower_indeces);
+thrust = thrust(lower_indeces);
     
 % Define custom model, e.g., exponential model: thrust = a * exp(b * throttle)
 [throttle_thrust, gof_tht] = fit(throttle, thrust, 'poly3');
 [throttle_power, gof_thp] = fit(throttle, pwr, 'poly3');
 [thrust_power, gof_tp] = fit(thrust, pwr, 'poly2');
-gof_thp
+% gof_thp
 % gof_tp
 % gof_tht
 % Plot data and fit
+
+usable_battery_watthr = 100; % placeholder
+flight_duration_data = usable_battery_watthr ./ pwr;
+% Define the custom fit type as 1 / (a*x + b)
+fitType = fittype('1 / (a * x + b)', 'independent', 'x', 'coefficients', {'a', 'b'});
+
+% Fit the data
+[flight_duration, gof_fd] = fit(throttle, flight_duration_data, fitType);
+gof_fd
+
 figure;
-plot(throttle_power, throttle, pwr);
+plot(flight_duration, throttle, flight_duration_data);
+% plot(throttle_power, throttle, pwr);
 xlabel('Throttle (%)');
 ylabel('Thrust (N)');
 title('Throttle vs. Thrust with Fit');
